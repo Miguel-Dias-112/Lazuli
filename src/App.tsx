1255b182e55e28e972b9f95471595d408cbf6236
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-11-25 16:30:36 -0300

    cria pasta isolada pro componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { Route, Router, Routes } from 'react-router-dom'
-
+import './App.css'
+import PaginaInicial from './Componentes/paginaInicial/PaginaInicial'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Routes>
-      <Route  path="/lazuli/" element={<h1>A</h1>}/>
+      <Route  path="/lazuli/" element={<PaginaInicial></PaginaInicial>}/>
       <Route  path="/lazuli/B" element={<h1>B</h1>}/>
       <Route  path="/lazuli/C" element={<h1>C</h1>}/>
   </Routes>
---
+++ src/Componentes/paginaInicial/PaginaInicial.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './PaginaInicial.css'
+function PaginaInicial() {
+
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  
+  )
+}
+
+export default PaginaInicial
