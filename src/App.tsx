e1c40b346ba52c1e0dc5e09837aeaa5da05156da
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-11-25 16:16:05 -0300

    configura react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import { Route, Router, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <header>
-    </header>
-    </>
+    <Routes>
+      <Route  path="/lazuli/" element={<h1>A</h1>}/>
+      <Route  path="/lazuli/B" element={<h1>B</h1>}/>
+      <Route  path="/lazuli/C" element={<h1>C</h1>}/>
+  </Routes>
   )
 }
 
