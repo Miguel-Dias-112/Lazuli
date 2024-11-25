f6589e6cca9727188aa5fe8eeb91038184d2c99a
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-11-25 19:57:40 -0300

    Estiliza Pagina de Fases

---
+++ src/Componentes/fasesView/FasesView.tsx
@@ -0,0 +1,34 @@
+import './FasesView.css'
+function FasesView() {
+  return (
+    <div className='FasesCtn'>
+        <header>
+            <button className='Icon'>
+                Icon
+            </button>
+            <h1>
+                Lazuli
+            </h1>
+        </header>
+        <main>
+            <h1> <div className='BlueBar'></div>Selecione uma fase<div className='BlueBar'></div></h1>
+            <div className='CarroselCtn' >
+              {
+                [1,2,3,4,5,6].map((item, index) => {
+                  return (
+                    <div className='CarroselItem' key={index}>
+                      <div className='Ball'></div>
+                      <p>Fase {item}</p>
+                      <button>Start</button>
+                    </div>
+                  )
+                })
+              }
+            </div>
+        </main>
+    </div>
+  
+  )
+}
+
+export default FasesView
